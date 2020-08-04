6372bdd56e62a7e1b8e106e00d7ddef87878d7e4
manasvi762 <64463973+manasvi762@users.noreply.github.com>
2020-08-04 20:03:23 +0530

    Create ese.js

---
+++ ese.js
@@ -0,0 +1,11 @@
+
+<fb:login-button 
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
